3b86eca7029dfd624fd152ede89380e2a310e074
Ryo Manzoku <manzoku.ryo@gmail.com>
2025-08-09 13:16:58 +0900

    fix Monster Strike link

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           スイカゲームライク
         </Link>
         <Link
-          href="/monst-game/"
+          href="/monst-game"
           className="bg-blue-600 text-white px-4 py-2 rounded"
         >
           モンスト風ミニゲーム
